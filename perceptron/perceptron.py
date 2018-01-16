13adc946ffdb292b7ab17a5c1e80ba509a5f55bc
Felix <wy47@duke.edu>
2018-01-16 15:58:08 -0500

    plot

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,5 +1,20 @@
 # library
 import numpy as np
+import matplotlib.pyplot as plt
+
+def perceptron(S, I):
+    list_mistakes = []
+    w = [0 for d in range( len(S[0][0]) )]
+    for e in range(I):
+        mistakes = 0
+        for i in range(len(S)):
+            if np.dot(S[i][1], np.dot(w, S[i][0])) <= 0:
+                mistakes += 1
+                w += np.dot(S[i][1], S[i][0])
+        list_mistakes.append( mistakes / len(S) )
+    plt.figure()
+    plt.plot(range(I), list_mistakes)
+    plt.show()
 
 # import data from MNIST
 from tensorflow.examples.tutorials.mnist import input_data
@@ -13,12 +28,6 @@ for i in range( len(mnist.train.labels) ):
     elif mnist.train.labels[i] == 9:
         S.append( (mnist.train.images[i], 1) ) #classify 9 to 1
 
-def perceptron(S, I):
-    w = [0 for d in range( len(S[0][0]) )]
-    for e in range(I):
-        for i in range(len(S)):
-            if np.dot(S[i][1], np.dot(w, S[i][0])) <= 0:
-                w += np.dot(S[i][1], S[i][0])
-    print(w)
+
 
 perceptron(S, 100)
