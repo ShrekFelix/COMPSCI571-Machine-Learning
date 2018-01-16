2da0887eff4318736ae6505f9faaa8771d481fee
Felix Yan <felixyan@Felixs-MacBook-Air.local>
2018-01-16 00:03:03 -0500

    compile

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -7,18 +7,18 @@ mnist = input_data.read_data_sets("MNIST_data/")
 
 # filter for 4 and 9
 S=[]
-for i in range( len(mnist.train.label) ):
-    if mnist.train.label[i] == 4:
-        S.append( (mnist.train.image[i], -1) ) #classify 4 to -1
-    elif mnist.train.label[i] == 9:
-        S.append( (mnist.train.image[i], 1) ) #classify 9 to 1
+for i in range( len(mnist.train.labels) ):
+    if mnist.train.labels[i] == 4:
+        S.append( (mnist.train.images[i], -1) ) #classify 4 to -1
+    elif mnist.train.labels[i] == 9:
+        S.append( (mnist.train.images[i], 1) ) #classify 9 to 1
 
 def perceptron(S, I):
     w = [0 for d in range( len(S[0][0]) )]
     for e in range(I):
         for i in range(len(S)):
-            if dot(S[i][1], dot(w, S[i][0])) <= 0:
-                w += dot(S[i][1], S[i][0])
+            if np.dot(S[i][1], np.dot(w, S[i][0])) <= 0:
+                w += np.dot(S[i][1], S[i][0])
     print(w)
 
 perceptron(S, 100)
