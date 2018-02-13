08f9bec6b62c2fdb55324188ef51ba8e0e6d0d7f
Felix <wy47@duke.edu>
2018-02-13 11:09:21 -0500

    update

--- DecisionTree/stump.m
+++
@@ -1,9 +0,0 @@
-clear;
-X = csvread('train.csv',1,0,[1 0 500 4]);
-Y = csvread('train.csv',1,5);
-
-%%
-mdl = fitctree(X,Y,'Surrogate','on','MaxNumSplits',1,'CrossVal','on');
-%% 
-
-view(mdl.Trained{1},'Mode','graph')
