55d811aa9e8e482c17648d933e8ba07742ec6ffd
Felix <wy47@duke.edu>
2018-02-20 21:46:32 -0500

    clean

--- random forest/load.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def load_CSV(file_name):
-    tmp = np.loadtxt(file_name, dtype=np.str, delimiter=",")
-    features = tmp[1:,:-1].astype(np.float)# load features
-    labels = tmp[1:,-1].astype(np.float)# load labels
-    return features, labels # return ndarray
