835fda14dfaf9eda96e8de184fea18c70fd313cb
Felix <wy47@duke.edu>
2018-02-28 20:07:59 -0500

    reorder

--- SVM classifier/svc.py
+++ SVM classifier/svc.py
@@ -2,69 +2,6 @@ import matplotlib.pyplot as plt
 from cvxopt import matrix, solvers
 import numpy as np
 
-def load_CSV(file_name):
-    tmp = np.loadtxt(file_name, dtype=np.str, delimiter=",")
-    features = tmp[1:,:-1].astype(np.float)# load features
-    labels = tmp[1:,-1].astype(np.float)# load labels
-    return features, labels # return ndarray
-
-def scale(data):
-    return (data - data.mean()) / data.std()
-
-def ConfusionMatrix(Y_hat, Y):
-    TP = 0
-    FP = 0
-    FN = 0
-    TN = 0
-    for i in range(len(Y)):
-        if Y_hat[i]==Y[i]:
-            if Y_hat[i]==1:
-                TP += 1
-            else:
-                TN += 1
-        else:
-            if Y_hat[i]==1:
-                FP += 1
-            else:
-                FN += 1
-                
-    CM = {
-        'TP':TP,
-        'FP':FP,
-        'FN':FN,
-        'TN':TN
-    }
-    return CM     
-
-def ROC(X, Y, classifier):
-    scores = classifier.decision_function(X)
-    curve = sorted(zip(scores,Y), key=lambda x:x[0], reverse=True)
-    x = [0 for i in range(len(curve))]
-    y = [0 for i in range(len(curve))]
-    for i in range(1,len(curve)):
-        if curve[i][1]==1:
-            x[i] = x[i-1]
-            y[i] = y[i-1]+1/len(Y[Y==1])
-        else:
-            x[i] = x[i-1]+1/len(Y[Y==0])
-            y[i] = y[i-1]  
-    plt.plot(x,y)
-    plt.xlim(0,1)
-    plt.ylim(0,1)   
-    return x,y
-
-def AUC(x,y):
-    area = 0
-    for i in range(1,len(x)):
-        area += (x[i] - x[i-1]) * y[i]
-    return area
-
-def radial_basis_kernel(x,z,sigma_sqr=25):
-    return np.exp(- np.linalg.norm(x-z) / sigma_sqr)
-
-
-
-
 
 class SVC():
     '''
@@ -132,3 +69,71 @@ predict(X_test)
         
         Y_hat = [1 if score>0 else -1 for score in scores]
         return Y_hat
+
+
+
+
+
+def load_CSV(file_name):
+    tmp = np.loadtxt(file_name, dtype=np.str, delimiter=",")
+    features = tmp[1:,:-1].astype(np.float)# load features
+    labels = tmp[1:,-1].astype(np.float)# load labels
+    return features, labels # return ndarray
+
+def scale(data):
+    return (data - data.mean()) / data.std()
+
+def ConfusionMatrix(Y_hat, Y):
+    TP = 0
+    FP = 0
+    FN = 0
+    TN = 0
+    for i in range(len(Y)):
+        if Y_hat[i]==Y[i]:
+            if Y_hat[i]==1:
+                TP += 1
+            else:
+                TN += 1
+        else:
+            if Y_hat[i]==1:
+                FP += 1
+            else:
+                FN += 1
+                
+    CM = {
+        'TP':TP,
+        'FP':FP,
+        'FN':FN,
+        'TN':TN
+    }
+    return CM     
+
+def ROC(X, Y, classifier):
+    scores = classifier.decision_function(X)
+    curve = sorted(zip(scores,Y), key=lambda x:x[0], reverse=True)
+    x = [0 for i in range(len(curve))]
+    y = [0 for i in range(len(curve))]
+    for i in range(1,len(curve)):
+        if curve[i][1]==1:
+            x[i] = x[i-1]
+            y[i] = y[i-1]+1/len(Y[Y==1])
+        else:
+            x[i] = x[i-1]+1/len(Y[Y==0])
+            y[i] = y[i-1]  
+    plt.plot(x,y)
+    plt.xlim(0,1)
+    plt.ylim(0,1)   
+    return x,y
+
+def AUC(x,y):
+    area = 0
+    for i in range(1,len(x)):
+        area += (x[i] - x[i-1]) * y[i]
+    return area
+
+def radial_basis_kernel(x,z,sigma_sqr=25):
+    return np.exp(- np.linalg.norm(x-z) / sigma_sqr)
+
+
+
+
